8b7d604a5375e3abab76d2493aa2d8ea103684ad
lucastercas <lucastercas@gmail.com>
2018-06-11 16:53:02 -0300

    Fix issue on parsing of link

--- src/parser/grammar.lua
+++ src/parser/grammar.lua
@@ -42,6 +42,7 @@ grammar = {
    *(V"PresentationElement"+V"Port"+V"Property"+V"Link"+V"MacroCall"+V"Template"+V"Spc")^0 *C(V"End"), false),
 
    Link = V"Spc"^0*pT.makeLink((V"Condition" *V"Spc"^1* ((V"Property"+V"Action")-V"End")^0 *C(V"End")*V"Spc"^0), false),
+
    Condition = pT.makeBind(V"ConditionId" *V"Spc"^1* (V"RepeatCondition"+V"Spc")^0 *P"do","condition"),
    ConditionId = pT.makeRelationship(C(V"Reserved") *V"Spc"^1* (C(V"Id")*(P"."*C(V"Id"))^-1)),
    RepeatCondition = P"and" *V"Spc"^1* V"ConditionId",
@@ -53,6 +54,7 @@ grammar = {
    MacroPresentationElement = V"Spc"^0* pT.makePresentationElement(C(V"Reserved") *V"Spc"^1 * C(V"Id") *V"Spc"^1
    *(V"MacroPresentationElement"+V"Port"+V"Property"+V"Link"+V"Template"+V"MacroCall"+V"Spc")^0 *C(V"End"), true),
    MacroLink = (V"Spc"^0*pT.makeLink((V"Condition" *V"Spc"^1* ((V"Property"+V"Action")-V"End")^0 *C(V"End")*V"Spc"^0), true)),
+
    Macro = V"Spc"^0* pT.makeMacro(P"macro" *V"Spc"^1* C(V"Id") *V"Spc"^0* V"Parameters"
       *V"Spc"^1* V"MacroBody"^-1 *V"Spc"^0* C(V"End") ),
    Parameters = P"("*  Ct(Cg( Ct(V"FieldParameters"^-1 * (',' * V"FieldParameters")^0),"parameters")) * P')',
@@ -71,7 +73,7 @@ grammar = {
    Template = V"Spc"^0*pT.makeTemplate(V"For"*V"Spc"^1*V"MacroCall"^0*V"Spc"^0* C(V"End")),
    For = (P"for" *V"Spc"^1* C(V"Lower"^1) *V"Spc"^0* P"="* V"Spc"^0*C(V"Digit"^1)*V"Spc"^0*P","*V"Spc"^0* P"#"*C((V"Lower"+V"Digit")^1) *V"Spc"^1* P"do"),
 
-   START = ((V"Spc"^0* Ct((V"Template"+V"Port"+V"Macro"+V"PresentationElement"+pT.makeLink(V"Link")+V"MacroCall")^0) * V"Spc"^0)* V"EOS"),
+   START = ((V"Spc"^0* Ct((V"Template"+V"Port"+V"Macro"+V"PresentationElement"+(V"Link")+V"MacroCall")^0) * V"Spc"^0)* V"EOS"),
 }
 
 
