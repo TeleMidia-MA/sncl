212a77c2d012920dde6bd167901f5d1e3f6fb97b
lucastercas <lucasmtercas@gmail.com>
2021-11-26 02:10:25 -0300

    task(cleanup): Remove luacov files

---
+++ spec/grammar_media_spec.lua
@@ -0,0 +1,72 @@
+local grammar = require('sncl.grammar')
+
+describe("#grammar #media element", function()
+
+  it("Should parse a empty #media", function()
+    local expected = {
+      presentation = {
+        testMedia = {
+          _type = "media",
+          id = "testMedia",
+          hasEnd = true,
+        }
+      }
+    }
+    local snclString = [[
+    media testMedia
+    end
+    ]]
+    local result = grammar.lpegMatch(snclString)
+    assert.are.same(expected.presentation, result.presentation)
+  end)
+
+  it("Should parse a #media with attributes", function()
+    local expected = {
+      presentation = {
+        testMediaAttributes = {
+          _type = "media",
+          id = "testMediaAttributes",
+          src = '"../images/testImage.png"',
+          descriptor = "testDescriptor",
+          hasEnd = true,
+        }
+      }
+    }
+    local snclString = [[
+    media testMediaAttributes
+      descriptor: testDescriptor
+      src: "../images/testImage.png"
+    end
+    ]]
+    local result = grammar.lpegMatch(snclString)
+    assert.are.same(expected.presentation, result.presentation)
+  end)
+
+  it("Should parse #media with properties", function ()
+    local expected = {
+      presentation = {
+        testMediaProperties = {
+          _type = "media",
+          id = "testMediaProperties",
+          hasEnd = true,
+          properties = {
+            focusIndex = "1",
+            focusBorderWidth = "3",
+            right = "10%",
+          },
+        }
+      }
+    }
+    local snclString = [[
+    media testMediaProperties
+      focusIndex: 1
+      focusBorderWidth: 3
+      right: 10% 
+    end
+    ]]
+    local result = grammar.lpegMatch(snclString)
+    assert.are.same(expected.presentation, result.presentation)
+  end)
+
+end)
+
